3ea82d588f100771b25d03bccaec298d7ba9c333
Ashish Bhatia <ashish27@icloud.com>
2018-11-29 14:59:18 -0500

    Identification: 2
    Change-Id: Ibca7b79271fcae9126c01cc79ca6311e16446bf1

--- gittest/Controllers/HomeController.cs
+++ gittest/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace gittest.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application is in azure.";
 
             return View();
         }
